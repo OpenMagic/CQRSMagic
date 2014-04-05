6db61c527e816a038218452523b185b79cbf7218
Tim Murphy <tim@26tp.com>
2014-04-06 07:11:51 +1000

    Added CQRS.Example project.

---
+++ Projects/CQRS.Example/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CQRS.Example
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                WriteException(ex);
+            }
+
+            Pause();
+        }
+
+        private static void Pause()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Press Enter key to continue...");
+            Console.ReadLine();
+        }
+
+        private static void WriteException(Exception exception)
+        {
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(exception);
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+}
