051c733857c95bc2fff02cb918ceb1c1199c88dc
Tim Murphy <tim@26tp.com>
2014-04-09 06:31:59 +1000

    Added ISendCommandTasks documentation.

--- Projects/CQRS.Example/CQRS/Commands/ISendCommandTasks.cs
+++ Projects/CQRS.Example/CQRS/Commands/ISendCommandTasks.cs
@@ -4,6 +4,9 @@ using CQRS.Example.CQRS.Events;
 
 namespace CQRS.Example.CQRS.Commands
 {
+    /// <summary>
+    ///     Represents the tasks that are created by <see cref="ICommandSender.SendCommand" />.
+    /// </summary>
     public interface ISendCommandTasks
     {
         Task<IEnumerable<IEvent>> HandleCommand { get; }
