b81f347f181abef98c9bdad31c012ab81d94107c
Tim Murphy <tim@26tp.com>
2014-07-03 15:27:58 +1000

    Added 'Add contact' scenario to new '@Example' feature.

---
+++ Projects/CQRSMagic.Specifications/Features/@Example.feature
@@ -0,0 +1,11 @@
+﻿Feature: Example
+	In order to learn CQRSMagic
+	As a programmer
+	I want an "end to end" example
+
+Scenario: Add contact
+	Given contact's name is Tim
+	And their email address is tim@example.org
+	When I send AddContact command
+	Then ContactAdded event is added to the event store
+	And contact is added to Contacts table
---
+++ Projects/CQRSMagic.Specifications/Steps/ExampleSteps.cs
@@ -0,0 +1,39 @@
+﻿using OpenMagic.Exceptions;
+using TechTalk.SpecFlow;
+
+namespace CQRSMagic.Specifications.Steps
+{
+    [Binding]
+    public class ExampleSteps
+    {
+        [Given(@"contact's name is Tim")]
+        public void GivenContactSNameIsTim()
+        {
+            throw new ToDoException();
+        }
+
+        [Given(@"their email address is tim@example\.org")]
+        public void GivenTheirEmailAddressIsTimExample_Org()
+        {
+            throw new ToDoException();
+        }
+
+        [When(@"I send AddContact command")]
+        public void WhenISendAddContactCommand()
+        {
+            throw new ToDoException();
+        }
+
+        [Then(@"ContactAdded event is added to the event store")]
+        public void ThenContactAddedEventIsAddedToTheEventStore()
+        {
+            throw new ToDoException();
+        }
+
+        [Then(@"contact is added to Contacts table")]
+        public void ThenContactIsAddedToContactsTable()
+        {
+            throw new ToDoException();
+        }
+    }
+}
