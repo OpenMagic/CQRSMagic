eb3b1e62d69e2b42f438b93abd852a54eb72267f
Tim Murphy <tim@26tp.com>
2014-07-03 15:09:56 +1000

    Added specifications project.

---
+++ Projects/CommonAssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("OpenMagic")]
+[assembly: AssemblyProduct("CQRSMagic")]
+[assembly: AssemblyCopyright("Copyright © 2014 OpenMagic")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
