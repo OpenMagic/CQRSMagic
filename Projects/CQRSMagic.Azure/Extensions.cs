55ce92035cc845497ddcaecf2d9a91a75c2aac81
Tim Murphy <tim@26tp.com>
2014-08-06 06:45:38 +1000

    Refactoring to expose private methods.

---
+++ Projects/CQRSMagic.Azure/Extensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CQRSMagic.Azure
+{
+    public static class Extensions
+    {
+        public static Guid ToAggregateId(this string partitionKey)
+        {
+            return new Guid(partitionKey);
+        }
+
+        public static DateTimeOffset ToEventCreated(this string rowKey)
+        {
+            var cleanRowKey = rowKey.RemoveTransactionIndexFromRowKey();
+            var value = DateTimeOffset.Parse(cleanRowKey);
+
+            return value;
+        }
+
+        public static string RemoveTransactionIndexFromRowKey(this string rowKey)
+        {
+            var index = rowKey.LastIndexOf('-');
+            var value = rowKey.Substring(0, index);
+
+            return value;
+        }
+    }
+}
